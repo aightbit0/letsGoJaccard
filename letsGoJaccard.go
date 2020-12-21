f73021064f2c6bc61b8d46c37706c95c8d7167f5
aightbit0 <svenlatka@web.de>
2020-12-21 21:24:14 +0100

    code cleanup

--- letsGoJaccard.go
+++ letsGoJaccard.go
@@ -5,21 +5,21 @@ import (
 )
 
 //Jaccard ...
-func Jaccard(searchval string, stest string) float64 {
+func Jaccard(firstString string, secondString string) float64 {
 	intersection := make(map[string]bool)
 	union := make(map[string]bool)
-	val1 := strings.Split(searchval, "")
-	val2 := strings.Split(stest, "")
-	for i := 0; i < len(val1); i++ {
-		if union[val1[i]] != true {
-			union[val1[i]] = true
+	firstSplitted := strings.Split(firstString, "")
+	secondSplitted := strings.Split(secondString, "")
+	for i := 0; i < len(firstSplitted); i++ {
+		if union[firstSplitted[i]] != true {
+			union[firstSplitted[i]] = true
 		}
-		for u := 0; u < len(val2); u++ {
-			if val1[i] == val2[u] {
-				intersection[val1[i]] = true
+		for u := 0; u < len(secondSplitted); u++ {
+			if firstSplitted[i] == secondSplitted[u] {
+				intersection[firstSplitted[i]] = true
 			}
-			if union[val2[u]] != true {
-				union[val2[u]] = true
+			if union[secondSplitted[u]] != true {
+				union[secondSplitted[u]] = true
 			}
 		}
 	}
